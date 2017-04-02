57e16eba6326577f88a8ae288c18eb99fe8d6f56
MEDavy <MEDavy@users.noreply.github.com>
2017-04-02 12:56:48 -0600

    Update freecontactformsettings.php

--- freecontactformsettings.php
+++ freecontactformsettings.php
@@ -2,7 +2,7 @@
 
 $email_to = "maximiliandavy@gmail.com"; // your email address
 $email_subject = "New feedback from Tengwar Transcriber"; // email subject line
-$thankyou = "thankyou.htm"; // thank you page
+$thankyou = "more.html"; // thank you page
 
 // if you update the question on the form -
 // you need to update the questions answer below
