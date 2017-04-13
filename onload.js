466da826d1e877cbb7530dec2322861a335f4d90
MEDavy <MEDavy@users.noreply.github.com>
2017-04-13 11:08:27 -0600

    Update onload.js

--- onload.js
+++ onload.js
@@ -2,3 +2,10 @@ $(function(){
       $("#HEADER").load("/header.html"); 
       $(".jsrequired").css("background-color", "white");
 });
+
+function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+}
