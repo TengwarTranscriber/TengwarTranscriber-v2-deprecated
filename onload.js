be5a8d3b8e25f6fe12333e646b207a3af74ee745
MEDavy <MEDavy@users.noreply.github.com>
2017-04-14 13:29:28 -0600

    Update onload.js

--- onload.js
+++ onload.js
@@ -1,6 +1,7 @@
 $(function(){
       $("#HEADER").load("/header.html"); 
       $(".jsrequired").css("background-color", "white");
+	$('.slider').unslider();
 });
 
 function getUrlParameter(name) {
