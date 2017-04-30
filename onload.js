59021fdb57389205aed3376320c4e42c52d256ee
MEDavy <MEDavy@users.noreply.github.com>
2017-04-30 16:42:29 -0600

    Update onload.js

--- onload.js
+++ onload.js
@@ -1,13 +1,14 @@
 $(function(){
-      $("#HEADER").load("/header.html");
-      $("#ALERTS").load("/alerts.html");
-      $(".jsrequired").css("background-color", "white");
-      console.error("I never took the time to fix the two errors involving 'unslider.css', but I did take the time to write code that writes error messages explaining the error messages that I didn't take the time to fix.");
-      console.error("Now I took the time to fix the errors but I didn't take the time to remove the code that writes the error messages explaining the error messages that I previously didn't take the time to fix; however I did take the time to write an error message explaining the error message explaining the error message that wasn't fixed when I wrote the first error message but was fixed by the time I wrote the second error message.");
-      console.log("%cWhat are you doing looking at the console? My code is no secret. Go to https://www.github.com/tengwartranscriber/tengwartranscriber.github.io to see it. Incidentally, the code that just ran was $('#HEADER').load('/header.html'); $('.jsrequired').css('background-color', 'white');", "background: black; color: white; font-size: 20px");
-      $(".secondary-header").click(function(){
-        $('html, body').animate({ scrollTop: 0 }, 'fast');
-      });
+  $("#HEADER").load("/header.html");
+  $("#ALERTS").load("/alerts.html");
+  $(".jsrequired").css("background-color", "white");
+  console.log("%cTo see source code, go to https://www.github.com/tengwartranscriber/tengwartranscriber.github.io to see it. Incidentally, the code that just ran was $('#HEADER').load('/header.html'); $('.jsrequired').css('background-color', 'white');", "background: black; color: white; font-size: 20px");
+  $(".secondary-header").click(function(){
+    $('html, body').animate({ scrollTop: 0 }, 'fast');
+  });
+  window.applicationCache.addEventListener("error", function(e) {
+    alert("You seem to be offline.\nAny pages on this site you have previously visited from this browser should still load.\nHowever, depending on your browser, they may render incorrectly.");
+  });
 });
 
 function getUrlParameter(name) {
